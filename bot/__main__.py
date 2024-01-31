66eda81abdb4937d19e04a30f5a7ddb00548b12b
Luger Shmitz <Zipliks@gmail.com>
2024-01-31 14:00:23 +0300

    Убрана поддержка вебхука

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,17 +1,14 @@
 import asyncio
 import logging
-
-from aiohttp import web
-from aiogram import Bot, Dispatcher
-from aiogram.client.telegram import TelegramAPIServer
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler
-from bot.handlers import setup_routers
-from fluent.runtime import FluentLocalization, FluentResourceLoader
-from bot.commandsworker import set_bot_commands
-from bot.middlewares import L10nMiddleware
 from pathlib import Path
 
-from bot.config_reader import config
+from aiogram import Bot, Dispatcher
+from fluent.runtime import FluentLocalization, FluentResourceLoader
+
+from bot.commandsworker import set_bot_commands
+from bot.config_reader import TOKEN
+from bot.handlers import setup_routers
+from bot.middlewares import L10nMiddleware
 
 
 async def main():
@@ -28,13 +25,13 @@ async def main():
     l10n_loader = FluentResourceLoader(str(locales_dir) + "/{locale}")
     l10n = FluentLocalization(["ru"], ["strings.ftl", "errors.ftl"], l10n_loader)
 
-    bot = Bot(token=config.bot_token.get_secret_value())
+    bot = Bot(token=TOKEN)
     dp = Dispatcher()
     router = setup_routers()
     dp.include_router(router)
 
-    if config.custom_bot_api:
-        bot.session.api = TelegramAPIServer.from_base(config.custom_bot_api, is_local=True)
+    # if config.custom_bot_api:
+    #     bot.session.api = TelegramAPIServer.from_base(config.custom_bot_api, is_local=True)
 
     # Регистрация мидлварей
     dp.update.middleware(L10nMiddleware(l10n))
@@ -43,31 +40,8 @@ async def main():
     await set_bot_commands(bot)
 
     try:
-        if not config.webhook_domain:
-            await bot.delete_webhook()
-            await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
-        else:
-            # Выключаем логи от aiohttp
-            aiohttp_logger = logging.getLogger("aiohttp.access")
-            aiohttp_logger.setLevel(logging.CRITICAL)
-
-            # Установка вебхука
-            await bot.set_webhook(
-                url=config.webhook_domain + config.webhook_path,
-                drop_pending_updates=True,
-                allowed_updates=dp.resolve_used_update_types()
-            )
-
-            # Создание запуска aiohttp
-            app = web.Application()
-            SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=config.webhook_path)
-            runner = web.AppRunner(app)
-            await runner.setup()
-            site = web.TCPSite(runner, host=config.app_host, port=config.app_port)
-            await site.start()
-
-            # Бесконечный цикл
-            await asyncio.Event().wait()
+        await bot.delete_webhook()
+        await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
     finally:
         await bot.session.close()
 
--- bot/config_reader.py
+++ bot/config_reader.py
@@ -1,22 +1,11 @@
-from typing import Optional
+import os
 
-from pydantic import BaseSettings, SecretStr
+from dotenv import load_dotenv
 
+load_dotenv()
 
-class Settings(BaseSettings):
-    bot_token: SecretStr
-    admin_chat_id: int
-    remove_sent_confirmation: bool
-    webhook_domain: Optional[str]
-    webhook_path: Optional[str]
-    app_host: Optional[str] = "0.0.0.0"
-    app_port: Optional[int] = 9000
-    custom_bot_api: Optional[str]
-
-    class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
-        env_nested_delimiter = '__'
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("ADMIN_CHAT_ID")
+REMOVE_SENT_CONFIRMATION = True
 
 
-config = Settings()
